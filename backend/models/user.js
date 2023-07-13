965843bbadeed6b6c616a596cc001f25a7f85812
Tatiana <tatiana.leclerc@gmail.com>
2023-07-13 23:23:43 +0100

    added some comments to code

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,60 +1,103 @@
+// Importing the fs module for file system operations
 const fs = require("fs");
+
+// Importing the uuid module to generate unique identifiers
 const { v4: uuidv4 } = require("uuid");
 
+// Setting the file path for the user data
 const userDataPath = "userData.json";
 
+// Defining a User class
 class User {
+  // Method to get all users
   static getUsers() {
+    // Reading the user data from the file
     const rawData = fs.readFileSync(userDataPath);
     const data = JSON.parse(rawData);
+
+    // Returning the array of users from the data
     return data.users;
   }
 
+  // Method to get a user by their ID
   static getUserById(userId) {
+    // Reading the user data from the file
     const rawData = fs.readFileSync(userDataPath);
     const data = JSON.parse(rawData);
+
+    // Finding the user with the specified ID and returning it
     return data.users.find((user) => user.userId === userId);
   }
 
+  // Method to create a new user
   static createUser(newUser) {
+    // Reading the user data from the file
     const rawData = fs.readFileSync(userDataPath);
     const data = JSON.parse(rawData);
 
+    // Generating a unique ID for the new user
     newUser.userId = uuidv4();
+
+    // Adding the new user to the data
     data.users.push(newUser);
 
+    // Writing the updated user data back to the file
     fs.writeFileSync(userDataPath, JSON.stringify(data));
 
+    // Returning the newly created user
     return newUser;
   }
 
+  // Method to update a user by their ID
   static updateUser(userId, updatedUser) {
+    // Reading the user data from the file
     const rawData = fs.readFileSync(userDataPath);
     const data = JSON.parse(rawData);
 
+    // Finding the index of the user with the specified ID
     const userIndex = data.users.findIndex((user) => user.userId === userId);
+
+    // Checking if the user exists
     if (userIndex !== -1) {
+      // Updating the user with the new information
       data.users[userIndex] = { ...data.users[userIndex], ...updatedUser };
+
+      // Writing the updated user data back to the file
       fs.writeFileSync(userDataPath, JSON.stringify(data));
+
+      // Returning the updated user
       return data.users[userIndex];
     }
 
+    // If the user does not exist, return null
     return null;
   }
 
+  // Method to delete a user by their ID
   static deleteUser(userId) {
+    // Reading the user data from the file
     const rawData = fs.readFileSync(userDataPath);
     const data = JSON.parse(rawData);
 
+    // Finding the index of the user with the specified ID
     const userIndex = data.users.findIndex((user) => user.userId === userId);
+
+    // Checking if the user exists
     if (userIndex !== -1) {
+      // Removing the user from the data array
       const deletedUser = data.users.splice(userIndex, 1);
+
+      // Writing the updated user data back to the file
       fs.writeFileSync(userDataPath, JSON.stringify(data));
+
+      // Returning the deleted user
       return deletedUser[0];
     }
 
+    // If the user does not exist, return null
     return null;
   }
 }
 
+// Exporting the User class
 module.exports = User;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,30 @@
+// Importing the express module
 const express = require("express");
+
+// Creating a router object
 const router = express.Router();
+
+// Importing the authMiddleware module
 const { middleware } = require("../middlewares/authMiddleware");
+
+// Importing the userController module
 const userController = require("../controllers/userController");
 
 // Define routes for users
+// GET request to retrieve all users
 router.get("/", middleware, userController.getAllUsers);
+
+// GET request to retrieve a user by ID
 router.get("/:id", middleware, userController.getUserById);
+
+// POST request to create a new user
 router.post("/", middleware, userController.createUser);
+
+// PUT request to update a user by ID
 router.put("/:id", middleware, userController.updateUser);
+
+// DELETE request to delete a user by ID
 router.delete("/:id", middleware, userController.deleteUser);
 
+// Exporting the router
 module.exports = router;
