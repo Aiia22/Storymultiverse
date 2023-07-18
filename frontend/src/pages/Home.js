f01f7c5a6ad4f204af9df4c11eeab98240f90e15
Shhanus <Shaneanslow8@gmail.com>
2023-07-18 18:08:00 +0100

    Implementation of home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,14 +3,23 @@ import React from "react";
 export default function Home() {
   
   return (
-    <div>
-      <h1>Hello Multiverse!</h1>
-      <p>
-        Welcome to the StorymultiverseApp. Explore the wonders of the
-        multiverse!
-      </p>
-    </div>
-  );
+    <><div>
+      <h1>StoryMultiverse</h1>
+      
+    </div><div className="landing-page">
+        <div className="background-image"></div>
+        <div className="content">
+          <p className="introduction">
+          Welcome to the StorymultiverseApp, where you can 
+          explore different forms of adaptations inspired by your favorite stories.
+          </p>
+          <div className="buttons">
+            <button className="explore-button">Explore Now</button>
+            <button className="login-button">Login</button>
+          </div>
+        </div>
+      </div></>
+);
 }
 
 //------- Copyright & references ---------------
