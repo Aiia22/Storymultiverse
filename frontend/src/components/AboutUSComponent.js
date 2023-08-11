e877d3976ff06959ad575cb3d53cdd0bd9fa86eb
Shhanus <Shaneanslow8@gmail.com>
2023-08-11 19:47:18 +0100

    About Us Page implemented

---
+++ frontend/src/components/AboutUSComponent.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div className="about-us">
+            <h1>About Us</h1>
+            <p>Welcome to StoryMultiverse, where we bring stories to life in endless adaptations!</p>
+            <p>Our mission is to provide a platform that allows users to explore and engage with adaptations inspired by various stories from books, movies, games, and comics. We believe that every story has the potential to be retold in unique and exciting ways.</p>
+            <p>At StoryMultiverse, you can discover adaptations that offer fresh perspectives, uncover hidden details, and delve into alternate storylines. Whether you're a fan of classic tales or seeking something entirely new, our platform has something for everyone.</p>
+            <p>Our team of passionate storytellers, developers, and creators work tirelessly to curate an ever-expanding collection of adaptations that cater to diverse tastes and interests. We value creativity, imagination, and the power of storytelling to connect people across cultures and backgrounds.</p>
+            <p>We're excited to have you on this journey through the StoryMultiverse, where the possibilities are as limitless as the stories themselves!</p>
+
+            {/* Pricing Plans Section */}
+            <div className="pricing-plans">
+                <h2>Pricing Plans</h2>
+                <div className="plan">
+                    <h3>StoryMultiVerse Free Plan</h3>
+                    <p>Enjoy unlimited access to our vast collection of adaptations.</p>
+                    <p>Dive into the world of stories, explore, and cross-reference various creative works.</p>
+                    <p>Connect with our passionate community and share your thoughts and discoveries.</p>
+                    <p>Free - No Subscription Required</p>
+                </div>
+                <div className="plan">
+                    <h3>StoryMultiVerse Infinity Plan</h3>
+                    <p>Unlock the full potential of Storyverse with our Infinity offering.</p>
+                    <p>Dive into the world of stories, explore, and cross-reference various creative works.</p>
+                    <p>Connect with our passionate community and share your thoughts and discoveries.</p>
+                    <p>Gain exclusive benefits, including access to free e-books, discounts with our partner platforms, and enhanced personalized recommendations.</p>
+                    <p>Immerse yourself in a seamless and ad-free experience.</p>
+                    <p>Embrace the joy of unlimited exploration and indulge in the vast tapestry of adaptations.</p>
+                    <p>€9.99/Month</p>
+                </div>
+                <div className="plan">
+                    <h3>StoryMultiVerse Infinity & Beyond Plan</h3>
+                    <p>Unlock the full potential of Storyverse with our Infinity offering.</p>
+                    <p>Dive into the world of stories, explore, and cross-reference various creative works.</p>
+                    <p>Connect with our passionate community and share your thoughts and discoveries.</p>
+                    <p>Gain exclusive benefits, including access to free e-books, discounts with our partner platforms, and enhanced personalized recommendations.</p>
+                    <p>Immerse yourself in a seamless and ad-free experience.</p>
+                    <p>Embrace the joy of unlimited exploration and indulge in the vast tapestry of adaptations.</p>
+                    <p>Early Acess to new adaptaions</p>
+                    <p>€12.99/Month</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AboutUs;
